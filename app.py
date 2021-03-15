24fa39d64e5e2cd21d027499911cfbd6a7c76e4b
sebstrug <sebstrug@users.noreply.github.com>
2021-03-15 11:48:11 +0000

    Fix possible error when running from localhost

--- app.py
+++ app.py
@@ -11,10 +11,16 @@ def home():
     ip_addr = request.remote_addr
     resp = requests.get(f"https://geolocation-db.com/json/{ip_addr}")
     data = resp.json()
-    lat, long = data.get("latitude"), data.get("longitude")
-    lover_city = find_city.find_my_city(lat, long)
+    your_city = data.get('city', 'London')
+    try:
+        lat_str, lng_str = data.get("latitude"), data.get("longitude")
+        lat, lng = float(lat_str), float(lng_str)
+    # handle no lat/long returned, default to London
+    except ValueError:
+        lat, lng = 51.5074, 0.1278
+    lover_city = find_city.find_my_city(lat, lng)
     return render_template(
-        "home.html", your_city=data.get("city", ""), lover_city=lover_city
+        "home.html", your_city=your_city, lover_city=lover_city
     )
 
 
